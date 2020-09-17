801dd5b49b36bb19ae8a900cfba7489a774e2ec6
Jazz <1337jazz@gmail.com>
2020-09-17 21:00:13 +0200

    Add project files.

---
+++ SM Programming Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SM_Programming_Exercise
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
